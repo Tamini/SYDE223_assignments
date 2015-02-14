16f546d7cac6fd43e26b9a6af3067a7815febaa8
Keren Chandran <keren.chandran@gmail.com>
2015-02-13 21:56:06 -0500

    Fixed bugs
    Removed lower bound index check
    Modified main and test files to include my tests
    
    New bug found in remove_back

--- assignment_1/a1_main.cpp
+++ assignment_1/a1_main.cpp
@@ -18,20 +18,24 @@ int main()
     bool seq_test1_passed = seq_test.test1();
 	bool seq_test2_passed = seq_test.test2();
     bool seq_test3_passed = seq_test.test3();
+    bool seq_test4_passed = seq_test.test4();
 
     bool linked_test1_passed = linked_test.test1();
     bool linked_test2_passed = linked_test.test2();
     bool linked_test3_passed = linked_test.test3();
+    bool linked_test4_passed = linked_test.test4();
 
     cout << "---Sequential List Tests---" << endl;
     cout << "Test1: " << get_status_str(seq_test1_passed) << endl;
  	cout << "Test2: " << get_status_str(seq_test2_passed) << endl;
     cout << "Test3: " << get_status_str(seq_test3_passed) << endl;
+    cout << "Test4: " << get_status_str(seq_test4_passed) << endl;
     cout << endl;
     cout << "---Linked List Tests---" << endl;
     cout << "Test1: " << get_status_str(linked_test1_passed) << endl;
     cout << "Test2: " << get_status_str(linked_test2_passed) << endl;
     cout << "Test3: " << get_status_str(linked_test3_passed) << endl;
+    cout << "Test4: " << get_status_str(linked_test4_passed) << endl;
 
     system("pause");
 }
--- assignment_1/a1_tests.hpp
+++ assignment_1/a1_tests.hpp
@@ -7,8 +7,6 @@
 #define ASSERT_TRUE(T) if (!(T)) return false;
 #define ASSERT_FALSE(T) if ((T)) return false;
 
-#include <cstdlib>
-#include <iostream>
 class SequentialListTest
 {
 public:
@@ -16,13 +14,14 @@ public:
     {
         unsigned int capacity = 5;
         SequentialList list(capacity);
+        
         ASSERT_TRUE(list.size() == 0)
         ASSERT_TRUE(list.capacity() == capacity)
         ASSERT_TRUE(list.empty() == true)
         ASSERT_TRUE(list.full() == false)
         return true;
     }
-
+    
     bool test2()
     {
         unsigned int capacity = 5;
@@ -30,14 +29,14 @@ public:
         SequentialList list2(capacity);
         ASSERT_TRUE(list1.insert_front(100))
         ASSERT_TRUE(list2.insert_back(100))
-    
+        
         ASSERT_TRUE(list1.size() == list2.size() && list1.size() == 1)
         ASSERT_TRUE(list1.data_ != NULL)
         ASSERT_TRUE(list2.data_ != NULL)
         ASSERT_TRUE(list1.select(0) == list2.select(0) && list1.select(0) == 100)
         return true;
     }
-
+    
     bool test3()
     {
         unsigned int capacity = 5;
@@ -50,13 +49,33 @@ public:
         ASSERT_TRUE(list.search(list.select(idx)) == idx)
         ASSERT_TRUE(list.select(capacity + 1) == list.select(capacity))
         ASSERT_TRUE(list.search(1000) == list.size())
-    
+        
         for (int i = 0; i < capacity; i++) {
             ASSERT_TRUE(list.select(i) == i*100 && list.data_[i] == i*100)
         }
         return true;
     }
-
+    
+    bool test4(){
+        unsigned int capacity = 5;
+        SequentialList list(capacity);
+        for (int i = 0; i < capacity; i++) {
+            ASSERT_TRUE(list.insert_back(i*100))
+        }
+        
+        ASSERT_TRUE(list.remove(3));
+        ASSERT_TRUE(list.insert(1000,3));
+        ASSERT_TRUE(list.remove_front());
+        ASSERT_TRUE(list.insert_front(2000));
+        ASSERT_TRUE(list.remove_back());
+        ASSERT_TRUE(list.insert_back(3000));
+        ASSERT_TRUE(list.replace(2,5000));
+        list.print(); //2000 100 5000 1000 3000
+        std::cout << list.search(200); //5
+        std::cout<<std::endl;
+        return true;
+    }
+    
 };
 
 class DoublyLinkedListTest
@@ -72,13 +91,13 @@ public:
         ASSERT_TRUE(list.tail_ == NULL)
         return true;
     }
-
+    
     bool test2()
     {
         DoublyLinkedList list1, list2;
         list1.insert_front(100);
         list2.insert_back(100);
-   
+        
         ASSERT_TRUE(list1.size() == list2.size() && list1.size() == 1)
         ASSERT_TRUE(list1.head_ != NULL && list1.head_ == list1.tail_)
         ASSERT_TRUE(list2.head_ != NULL && list2.head_ == list2.tail_)
@@ -87,27 +106,55 @@ public:
         ASSERT_TRUE(list1.select(0) == list2.select(0) && list1.select(0) == 100)
         return true;
     }
-
+    
     bool test3()
     {
         DoublyLinkedList list;
         const int num_elems = 5;
         for (int i = 0; i < num_elems; i++) {
-            ASSERT_TRUE(list.insert_back(i*100))
+            ASSERT_TRUE(list.insert(i*100, i))
         }
         unsigned int idx = 3;
         ASSERT_TRUE(list.search(list.select(idx)) == idx)
-        ASSERT_TRUE(list.select(num_elems + 1) == NULL)
+        ASSERT_TRUE(list.select(num_elems + 1) == 400)
         ASSERT_TRUE(list.search(1000) == list.size())
-    
+        
         for (int i = 0; i < num_elems; i++) {
             ASSERT_TRUE(list.select(i) == i*100)
         }
-    
+        
         ASSERT_TRUE(list.head_->prev == NULL && list.tail_->next == NULL)
         return true;
     }
-
+    
+    bool test4()
+    {
+        DoublyLinkedList list;
+        const int num_elems = 5;
+        for (int i = 0; i < num_elems; i++) {
+            ASSERT_TRUE(list.insert(i*100, i))
+        }
+        list.print();
+        list.insert(600, 2);
+        ASSERT_TRUE(list.select(2) == 600);
+        list.insert_front(1000);
+        list.insert_back(7000);
+        list.remove(1);
+        
+        std::cout<<"INSERT 1000 and 7000 (front back respectively) and remove index 1 element";
+        std::cout<<std::endl;
+        
+        list.print();
+        list.replace(3,9000);
+        list.remove_front();
+        list.remove_back();
+        std::cout<<"Replaced element 4 with 9000 and removed first and last element";
+        std::cout<<std::endl;
+        list.print();
+        return true;
+    }
+    
+    
 };
 
 
