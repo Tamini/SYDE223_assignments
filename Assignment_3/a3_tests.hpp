538e383a82f7898dddc8ef0140b60a0899c62ed4
Tristan Amini <tristanamini@hotmail.com>
2015-04-06 18:09:04 -0400

    Finished 4th queue test.

--- Assignment_3/a3_tests.hpp
+++ Assignment_3/a3_tests.hpp
@@ -32,10 +32,10 @@ public:
     ASSERT_TRUE( q.size() == 1 );
     ASSERT_TRUE( q.max() == 10 );
 
-//    ASSERT_TRUE( q.dequeue() );
-//    ASSERT_TRUE( q.empty() );
-//    ASSERT_TRUE( !q.full() );
-//    ASSERT_TRUE( q.size() == 0 );
+    ASSERT_TRUE( q.dequeue() );
+    ASSERT_TRUE( q.empty() );
+    ASSERT_TRUE( !q.full() );
+    ASSERT_TRUE( q.size() == 0 );
     return true;
   }
 
@@ -64,11 +64,11 @@ public:
     ASSERT_TRUE( q.max() == 5 );
     ASSERT_TRUE( q.enqueue(4) );
     ASSERT_TRUE( q.max() == 5 );
-    q.print();
     ASSERT_FALSE( q.full() );
     ASSERT_TRUE( q.enqueue(10));
-    q.print();
-    //ASSERT_TRUE( q.max() == 10);
+    ASSERT_TRUE( q.max() == 10);
+    ASSERT_TRUE( q.dequeue() );
+    ASSERT_TRUE( q.max() == 5);
     return true;
   }
 };
@@ -116,11 +116,11 @@ public:
 
     BinarySearchTree bst;
     ASSERT_TRUE(bst.root_ == NULL);
-//    ASSERT_TRUE(bst.size_ == 0 && bst.size() == 0);
-//
-//    std::string tree_level_order = level_order(bst.root_);
-//    // Compare the tree's representation to the expected tree.
-//    ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
+    ASSERT_TRUE(bst.size_ == 0 && bst.size() == 0);
+
+    std::string tree_level_order = level_order(bst.root_);
+    // Compare the tree's representation to the expected tree.
+    ASSERT_TRUE(tree_level_order.compare(expected_tree_level_order) == 0)
     return true;
   }
 
